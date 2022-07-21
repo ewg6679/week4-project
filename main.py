40c309cd66261c4dd1d89a58aa072708e5663088
Liz <108296970+LizardRain@users.noreply.github.com>
2022-07-21 12:19:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'week4-project/static/images'
 app.config['SECRET_KEY'] = 'fec93d1b1cb7926beb25960608b25818'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-map_client = googlemaps.Client('AIzaSyBU105nhaExFWjtUldUDYwFxEKG5bogWPU')
 session = Session(engine)
 user_data = None
 
